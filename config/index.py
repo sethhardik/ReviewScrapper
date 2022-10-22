5f65127f11f6cde61f317b675b2e484abdc322ae
sethhardik <hseth469@gmail.com>
2022-10-23 03:24:30 +0530

    basic configuration added

--- config/index.py
+++ config/index.py
@@ -1,1 +1,17 @@
-# write config code here
+class ObjectRepo:
+    def __init__(self):
+        print("here")
+
+    def getUsernameforMongoDB(self):
+        username = "hardik"
+        return username
+
+    def getPasswordforMongoDB(self):
+        password = "hardik"
+        return password
+
+    def getLoggingPath(self):
+        logpath = "logs/savelogs/"
+        return logpath
+
+    
