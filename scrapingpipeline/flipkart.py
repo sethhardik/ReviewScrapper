d5fc83a154a7f83aa173c8ee379d2f82fc84e388
sethhardik <hseth469@gmail.com>
2022-10-23 03:31:17 +0530

    frontend folders added, scraping pipeline start

---
+++ scrapingpipeline/flipkart.py
@@ -0,0 +1,7 @@
+import random
+import pandas as pd
+# fetch mongodb class so that we can store data
+from db.index import MongoDBManagement
+#  fetch configuration
+from config.index import ObjectRepo
+# selenium based imports inorder to perform data fetching pipeline
